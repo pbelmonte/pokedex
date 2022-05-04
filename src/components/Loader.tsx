a8d61d72b8737ed9220e0b4e58ecac0b0a61150b
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2022-05-04 13:18:35 -0400

    chore: add gatsby-srr and remove public from gitignore

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -11,7 +11,13 @@ interface Props {
 function Loader({ loading }: Props) {
   return loading ? (
     <div className="m-auto">
-      <img className="loader" src={pokeball} alt="loading" width="50"  height="50" />
+      <img
+        className="loader"
+        src={pokeball}
+        alt="loading"
+        width="50"
+        height="50"
+      />
     </div>
   ) : (
     <div />
